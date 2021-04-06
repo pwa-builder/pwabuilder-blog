953fbeaa670555f32c6e67a9f17cffe11c7d8778
Harmandeep Pannu <harman.pannu@freshconsulting.com>
2021-04-06 01:50:29 -0700

    Added: tags,categories / improved: layouts names

--- .eleventy.js
+++ .eleventy.js
@@ -3,16 +3,26 @@ const siteSettings = require("./src/globals/site.json")
 module.exports = (config) => {
   config.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"))
   config.addPlugin(require("@11ty/eleventy-plugin-rss"))
-
   config.addFilter("dateDisplay", require("./filters/date-display.js"))
+  config.addFilter("filterTagList", (tags) => {
+    // should match the list in tags.njk
+    return (tags || []).filter(
+      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+    )
+  })
+  config.addCollection("tagList", function (collection) {
+    let tagSet = new Set()
+    collection.getAll().forEach((item) => {
+      ;(item.data.tags || []).forEach((tag) => tagSet.add(tag))
+    })
 
+    return [...tagSet]
+  })
   config.addPassthroughCopy({ public: "./" })
-
   config.setBrowserSyncConfig({
     files: ["dist/**/*"],
     open: true,
   })
-
   config.setDataDeepMerge(true)
 
   config.addCollection("postsWithoutDrafts", (collection) =>
@@ -29,7 +39,7 @@ module.exports = (config) => {
       includes: "includes",
       layouts: "includes/layouts",
       data: "globals",
-      assets: "assets"
+      assets: "assets",
     },
   }
 }
